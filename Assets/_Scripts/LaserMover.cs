a1fb3da66846a5b2bbd61f14f9f7c971806af0f1
Andrew Trinidad <atrinid7my.centennialcollege.ca>
2019-10-04 19:28:33 -0400

    Added Player and the ability to shoot lazers

---
+++ Assets/_Scripts/LaserMover.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserMover : MonoBehaviour
+{
+    // Variable Declarations
+    public float speed;
+
+    private Rigidbody2D rBody;
+
+    // Use this for initialization
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody2D>();
+        rBody.velocity = transform.right * speed;
+    }
+}
